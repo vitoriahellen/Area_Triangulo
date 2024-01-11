a5022c36d2ed9b2ea2ab542e8f33f43dc9c67910
Vitoria Hellen Ferreira Silva Nunes <50849837+vitoriahellen@users.noreply.github.com>
2024-01-11 11:36:41 -0300

    Update Triangulo.java

--- course/src/entities/Triangulo.java
+++ course/src/entities/Triangulo.java
@@ -1,33 +1,20 @@
 package entities;
 
 public class Triangulo {
-	private double a;
-	private double b;
-	private double c;
+	public double a;
+	public double b;
+	public double c;
 	
-	public Double getA () {
-		return a;
-	}
-	
-	public void setA(double a) {
-		this.a = a;
-	}
-
-	public Double getB () {
-		return b;
-	}
-	
-	public void set(double b) {
-		this.b = b;
-	}
-	
-	public Double getC () {
-		return c;
-	}
-	
-	public void setB(double c) {
-		this.c = c;
+	public double area() {
+		
+		double p = (a+b+c) / 2.0;
+		double result = Math.sqrt(p * (p-a) * (p-b) * (p-c));
+		return result; 
 	}
 	
 }
+	
+
+
+
 
